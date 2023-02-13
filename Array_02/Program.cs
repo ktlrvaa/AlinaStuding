b4114a208594d7f789d8f0f7c0f1a5037311424a
ktlrvaa <ktlrv@bk.ru>
2023-02-13 22:20:09 +0300

    Разработка логической игры "LogicPuzzles".

--- Array_02/Program.cs
+++ Array_02/Program.cs
@@ -8,7 +8,7 @@ namespace Array_02
         {
             Console.WriteLine("Выберите команду, которую нужно исполнить: \n 1. Вывести все элементы массива. \n 2. Вывести все элементы массива в обратном порядке. \n 3. Вывести четные элементы массива. \n 4. Вывести все элементы массива через 1. \n 5. Вывести все элементы массива пока не встретится элемент -1.");
             string key = Console.ReadLine();
-            int[] array = { -2, -1, 0, 1, 2, 3,4};
+            int[] array = { -2, -1, 0, 1, 2, 3, 4, 6 };
             while (key != "1"
                     && key != "2"
                 && key != "3"
@@ -21,16 +21,16 @@ namespace Array_02
             switch (key)
             {
                 case "1":
-                   
+
                     Console.WriteLine("Все элементы массива: ");
                     for (int i = 0; i < array.Length; i++)
                     {
-                        Console.WriteLine(i + ":"+ array[i]);
+                        Console.WriteLine(i + ":" + array[i]);
                     }
                     break;
-                    case "2":
+                case "2":
                     Console.WriteLine("Элементы массива в обратном порядке: ");
-                    for (int i = array.Length-1; i >= 0; i--)
+                    for (int i = array.Length - 1; i >= 0; i--)
                     {
                         Console.WriteLine(i + ":" + array[i]);
                     }
@@ -41,20 +41,20 @@ namespace Array_02
                     {
                         if (array[i] % 2 == 0)
                         {
-                        Console.WriteLine(i + ":" + array[i]);
+                            Console.WriteLine(i + ":" + array[i]);
                         }
                     }
                     break;
-                    case "4":
+                case "4":
                     Console.WriteLine("Элементы массива через 1.");
-                    for (int i = 0; i< array.Length; i+=2)
+                    for (int i = 0; i < array.Length; i += 2)
                     {
                         Console.WriteLine(i + ":" + array[i]);
                     }
                     break;
-                    case "5":
+                case "5":
                     Console.WriteLine("Все элементы массива до -1.");
-                        for (int i = 0; i < array.Length; i++)
+                    for (int i = 0; i < array.Length; i++)
                     {
                         if (array[i] >= 0)
                         {
---
+++ LogicPuzzles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace LogicPuzzles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решите логическую загадку. Вам дано пять попыток.");
+            Console.WriteLine("Для выхода из программы введите exit.");
+
+            Console.WriteLine("Загадка: На столе лежат две монеты, в сумме они дают 3 рубля. Одна из них - не 1 рубль. Какие это монеты?");
+
+            //Максимальное кол-во попыток
+            const int maxAttempt = 5;
+            //Счетчик попыток
+            int attempt = 0;
+            //Загаданный цвет
+            string coinsVarOne = "21";
+            string coinsVarTwo = "12";
+            string coinsVarFree = "2 и 1";
+            string coinsVarFour = "1 и 2";
+
+
+            while (attempt < maxAttempt)
+            {
+                attempt++;
+                Console.WriteLine("Попытка {0}:", attempt);
+                //Строка для ввода ответа
+                string value = Console.ReadLine();
+                //Условие для команды "Выход из игры"
+                if (value == "exit")
+                {
+                    //Завершаем цикл при выходе из игры
+                    break;
+                }
+                //Условие для игры
+                
+                if (value == coinsVarOne || value == coinsVarTwo || value == coinsVarFree || value == coinsVarFour)
+                { //Загадка решена
+                    Console.WriteLine("Поздравляем, вы решили загадку с {0} попытки!\nВ условии сказано, что только одна из монет не 1 рубль.", attempt);
+                    //Завершаем цикл
+                    break;
+                }
+                //Возвращаем к циклу при неверном вводе ответа
+                continue;
+               
+            }
+            //Попытки закончились 
+            if (attempt == maxAttempt)
+            {
+                Console.WriteLine("Попытки закончились.");
+
+            }
+            //Завершаем игру
+            Console.WriteLine("Конец игры!");
+            Console.ReadLine();
+        }
+    }
+
+}
