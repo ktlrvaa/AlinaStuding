3fc955eb46be58f275259a0f239d2ff33cdc4f69
ktlrvaa <ktlrv@bk.ru>
2023-02-23 21:44:02 +0300

    Практическое занятие по теме "Индексаторы".

---
+++ Indexers_01/Dictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexers_01
+{
+    internal class Dictionary
+    {
+        private string[] _original = new string[5];
+        private string [] _translation = new string[5];
+
+        public Dictionary()
+        {
+            _original[0] = "Бесконечность"; _translation[0] = "Infinity";
+            _original[1] = "Солнце"; _translation[1] = "Sun"; 
+            _original[2] = "Северное сияние"; _translation[2] = "Aurora";
+            _original[3] = "Шмель"; _translation[3] = "Bumblebee"; 
+            _original[4] = "Созвездие"; _translation[4] = "Constellation"; 
+        }
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < _original.Length; i++)
+
+                    if (_original[i] == index)
+                    return _original[i] + " - " + _translation[i];
+
+                return string.Format($"{index} - нет перевода для этого слова.");
+            }
+        }
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < _original.Length)
+                    return _original[index] + " - " + _translation[index];
+                else
+                    return "Попытка обращения за пределы массива.";
+            }
+        }
+
+    }
+}
---
+++ Indexers_01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Indexers_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary dictionary = new Dictionary();
+            first:
+            Console.WriteLine("Какое слово вы хотите перевети?" +
+               "\n1. Бесконечность. \n2. Солнце. \n3. Северное сияние. \n4. Шмель. \n5. Созвездие");
+            Console.WriteLine("Введите нужну цифру.");
+            string key = Console.ReadLine();
+            while (key != "1"
+                    && key != "2"
+                && key != "3"
+                && key != "4"
+                && key != "5")
+            {
+                Console.WriteLine("Вы ввели неверное значение.");
+                key = Console.ReadLine();
+            }
+            switch (key)
+            {
+                case "1":
+                    Console.WriteLine(dictionary["Бесконечность"]);
+                    break;
+                    case "2":
+                    Console.WriteLine(dictionary["Солнце"]);
+                    break;
+                    case "3":
+                    Console.WriteLine(dictionary["Северное сияние"]);
+                    break;
+                    case "4":
+                    Console.WriteLine(dictionary["Шмель"]);
+                    break;
+                    case "5":
+                    Console.WriteLine(dictionary["Созвездие"]);
+                    break;
+            }
+            Console.WriteLine("Вы хотите перевести еще слово?");
+            if (Console.ReadLine() == "да".ToLower() || Console.ReadLine() == "yes".ToLower())
+                goto first;
+        }
+
+    }
+}
