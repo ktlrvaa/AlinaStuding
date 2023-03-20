d181d1187260fe7e01cfaec298f71530a4fba97f
ktlrvaa <ktlrv@bk.ru>
2023-03-20 21:20:15 +0300

    Практическое занятие по теме "Списки"

---
+++ List_01/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List_01
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public long Articul { get; set; }
+        public double Price { get; set; }
+
+    }
+
+}
---
+++ List_01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var productList = new List<Product>();
+            Start:
+            while (true)
+            {
+                var product = new Product();
+                Console.WriteLine("Наименование товара:");
+                product.Name = Console.ReadLine();
+                Console.WriteLine("Артикул товара:");
+                var inputArticul = Console.ReadLine();
+                product.Articul = long.Parse(inputArticul);
+                Console.WriteLine("Стоимость товара в формате 00,00:");
+                product.Price = Convert.ToDouble(Console.ReadLine());
+                productList.Add(product);
+                Console.WriteLine("Если вы хотите завершить сканирование, то напишите команду exit.\n" +
+                    "Если хотите продолжить сканирование товаров, то напишите команду next.");
+                string comand = Console.ReadLine();
+                if (comand == "exit")
+                {
+                    Console.WriteLine("Сканирование завершено.");
+                    break;
+                }
+                if (comand == "next")
+                {
+                    goto Start;
+                }
+
+                Console.WriteLine("Команда введена неверно.");
+                
+            }
+        }
+    }
+}
